7efe341fa79578334c2fadfb35e9babe8a7dc1eb
Lena Köhler <lener.koehler@gmail.com>
2018-10-25 09:38:58 +0200

    Added Comments

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,43 +3,43 @@ import UIElements, {
 } from "./UIManager.js"
 
 var input = UIElements.input,
-    suggestfield = UIElements.suggestfield;
-
-var languagelist = {
-    "name": [
-        "Mandarin",
-        "Englisch",
-        "Hindi",
-        "Spanisch",
-        "Arabisch",
-        "Bengali",
-        "Portugiesich",
-        "Russisch",
-        "Deutsch",
-        "Japanisch",
-        "Französisch",
-        "Javanisch",
-        "Vietnam-esisch",
-        "Lahnda",
-        "Koreanisch",
-        "Telugu",
-        "Italienisch",
-        "Marathi",
-        "Urdu"
-    ]
-}
-
-var name = languagelist.name;
+    suggestfield = UIElements.suggestfield,
+    languagelist = {
+        "name": [
+            "Mandarin",
+            "Englisch",
+            "Hindi",
+            "Spanisch",
+            "Arabisch",
+            "Bengali",
+            "Portugiesich",
+            "Russisch",
+            "Deutsch",
+            "Japanisch",
+            "Französisch",
+            "Javanisch",
+            "Vietnam-esisch",
+            "Lahnda",
+            "Koreanisch",
+            "Telugu",
+            "Italienisch",
+            "Marathi",
+            "Urdu"
+        ]
+    },
+    name = languagelist.name;
 
 input.oninput = function () {
     var count = 0;
     var suggest = new Array(count);
+    //Find matching phrases and save them into a new Array
     for (var i = 0; i < name.length; i++) {
         if (name[i].includes(input.value) == true) {
-            suggest[count]=name[i];
+            suggest[count] = name[i];
             count++;
         }
     }
+    //show suggestions
     for (var i = 0; i < suggest.length; i++) {
         var field = document.createElement("div");
         field.className = "suggest-field";
@@ -48,13 +48,11 @@ input.oninput = function () {
         suggestfield[i].innerHTML = suggest[i];
     }
 
+    //Delete previous divs / suggestions
     input.onkeydown = function () {
-        console.log("change");
         var element = UIElements.suggestcontainer;
         while (element.firstChild) {
             element.removeChild(element.firstChild);
         }
     }
 }
-
-
